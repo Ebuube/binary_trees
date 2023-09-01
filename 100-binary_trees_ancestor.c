e3a3610ba7c730b03558e4897399d73107f51d12
Awopeju Kolawole Moses <Awopejumoses@gmail.com>
2023-09-01 17:24:11 +0100

    modifies

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,181 +1,26 @@
 #include "binary_trees.h"
-#include <stdio.h>	/* test */
 
 /**
- * binary_trees_ancestor - find the lowest common ancestor of two nodes
- * @first: a pointer to the first node
- * @second: a pointer to the second node
+ * binary_trees_ancestor - Finds the lowest common ancestor of two nodes.
+ * @first: Pointer to the first node.
+ * @second: Pointer to the second node.
  *
- * Return: a pointer to the lowest common ancestor if any,
- * else NULL
+ * Return: If no common ancestors return NULL, else return common ancestor.
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second)
 {
-	binary_tree_t *ancestor = NULL;
-	list_ancestors_t *f_ancestors_list = NULL, *s_ancestors_list = NULL;
+	binary_tree_t *firstnode, *secondnode;
 
-	if (first == NULL || first->parent == NULL ||
-		second == NULL || second->parent == NULL)
-	{/* nodes without parents can't have an ancestor */
+	if (!first || !second)
 		return (NULL);
-	}
+	if (first == second)
+		return ((binary_tree_t *)first);
 
-	f_ancestors_list = create_list_ancestors(first);
-	s_ancestors_list = create_list_ancestors(second);
-	if (f_ancestors_list == NULL || s_ancestors_list == NULL)
-	{/* Can't create parent's list */
-		return (NULL);
-	}
-	ancestor = (binary_tree_t *)find_first_match(f_ancestors_list,
-			s_ancestors_list);
-
-	free_ancestors_list(&f_ancestors_list);	/* free memory */
-	free_ancestors_list(&s_ancestors_list);	/* free memory */
-
-	return (ancestor);
-}
-
-/**
- * find_first_match - finds the first mathing ancestors in a list of ancestors
- * @list_1: first list of ancestors
- * @list_2: second list of ancestors
- *
- * Return: the first matching ancestors in the two starting from index 0
- * else NULL if not found
- */
-binary_tree_t *find_first_match(const list_ancestors_t *list_1,
-	const list_ancestors_t *list_2)
-{
-	const list_ancestors_t *tmp = NULL;
-
-	if (list_1 == NULL || list_2 == NULL)
-	{
-		return (NULL);
-	}
-
-	for (; list_1 != NULL; list_1 = list_1->next)
-	{
-		for (tmp = list_2; tmp != NULL; tmp = tmp->next)
-		{
-			if (list_1->p == tmp->p)
-			{
-				return ((binary_tree_t *)list_1->p);
-			}
-		}
-	}
-
-	return (NULL);	/* NO MATCH FOUND */
-}
-
-/**
- * find_ancestor - find a node's ancestor in an ancestor list
- * @node: node to find ancestor
- * @head: a list of ancestors
- *
- * Return: the matching ancestor if any else NULL
- */
-const binary_tree_t *find_ancestor(const binary_tree_t *node,
-	list_ancestors_t *head)
-{
-	const binary_tree_t *ancestor = NULL;
-	list_ancestors_t *tmp = NULL;
-
-	if (head == NULL || node == NULL || node->parent == NULL)
-	{
-		return (NULL);
-	}
-
-	while (node != NULL)
-	{
-		tmp = head;
-		while (tmp != NULL)
-		{
-			if (tmp->p == node->parent)
-			{
-				ancestor = tmp->p;
-				break;
-			}
-			tmp = tmp->next;
-		}
-
-		if (ancestor != NULL)
-		{/* found an ancestor */
-			break;
-		}
-		node = node->parent;
-	}
-
-	return (ancestor);
-}
-
-/**
- * create_list_ancestors - creat a list of ancestors of a node
- * @node: to work on
- *
- * Description: current node is included in the ancestor list
- *
- * Return: a pointer to the list else NULL
- */
-list_ancestors_t *create_list_ancestors(const binary_tree_t *node)
-{
-	list_ancestors_t *head = NULL, *tail = NULL, *tmp = NULL;
-
-	if (node == NULL)
-	{
-		return (NULL);
-	}
-
-	/* first node in list */
-	tmp = malloc(sizeof(list_ancestors_t));
-	if (tmp == NULL)
-	{
-		return (NULL);
-	}
-	tmp->p = node;
-	tmp->next = NULL;
-	head = tmp;
-
-	for (tail = tmp; node != NULL;)
-	{/* make list of ancestors - add node to end of list */
-		tmp = malloc(sizeof(list_ancestors_t));
-		if (tmp == NULL)
-			return (NULL);
-		tmp->p = node->parent;
-		tmp->next = NULL;
-		tail->next = tmp;
-		tail = tmp;
-		node = node->parent;
-	}
-
-	return (head);
-}
-
-
-/**
- * free_ancestors_list - safely free a list of ancestors
- * @head: address of the pointer to the head of the list
- *
- * Return: nothing
- */
-void free_ancestors_list(list_ancestors_t **head)
-{
-	list_ancestors_t *tmp = NULL;
-
-	if (head == NULL || (*head) == NULL)
-	{
-		return;
-	}
-
-	tmp = (*head)->next;
-	while ((*head) != NULL)
-	{
-		free((*head));
-		(*head) = tmp;
-
-		if (tmp != NULL)
-			tmp = tmp->next;
-	}
-
-	(*head) = NULL;	/* safety precaution */
+	firstnode = first->parent, secondnode = second->parent;
+	if (first == secondnode || !firstnode || (!firstnode->parent && secondnode))
+		return (binary_trees_ancestor(first, secondnode));
+	else if (firstnode == second || !secondnode || (!secondnode->parent && firstnode))
+		return (binary_trees_ancestor(firstnode, second));
+	return (binary_trees_ancestor(firstnode, secondnode));
 }
--- binary_trees.h
+++ binary_trees.h
@@ -1,12 +1,15 @@
-#ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
 #include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+/* Data structures */
 
 /**
  * struct binary_tree_s - Binary tree node
- *
  * @n: Integer stored in the node
  * @parent: Pointer to the parent node
  * @left: Pointer to the left child node
@@ -19,31 +22,26 @@ struct binary_tree_s
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
 };
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
 /**
- * struct list_ancestors_s - Linked list of ancestors of a node
- *
- * @p: address of an ancestor binary tree node
- * @next: pointer to next item in list
+ * struct levelorder_queue_s - Level order traversal queue.
+ * @node: A node of a binary tree.
+ * @next: The next node to traverse to in the binary tree.
  */
-typedef struct list_ancestors_s
+typedef struct levelorder_queue_s
 {
-	const struct binary_tree_s *p;
-	struct list_ancestors_s *next;
-} list_ancestors_t;
+	binary_tree_t *node;
+	struct levelorder_queue_s *next;
+} levelorder_queue_t;
 
-
-/* TYPEDEFINITIONS */
-
-/*
- * binary_tree_t - data type for binary trees
- */
-typedef struct binary_tree_s binary_tree_t;
-
+/* Printing helper function */
 void binary_tree_print(const binary_tree_t *);
 
-
-/* USER DEFINED FUNCTIONS */
+/* Task function prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -65,12 +63,24 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second);
-list_ancestors_t *create_list_ancestors(const binary_tree_t *node);
-const binary_tree_t *find_ancestor(const binary_tree_t *node,
-	list_ancestors_t *head);
-void free_ancestors_list(list_ancestors_t **head);
-binary_tree_t *find_first_match(const list_ancestors_t *list_1,
-	const list_ancestors_t *list_2);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+int binary_tree_is_complete(const binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+int binary_tree_is_bst(const binary_tree_t *tree);
+bst_t *bst_insert(bst_t **tree, int value);
+bst_t *array_to_bst(int *array, size_t size);
+bst_t *bst_search(const bst_t *tree, int value);
+bst_t *bst_remove(bst_t *root, int value);
+int binary_tree_is_avl(const binary_tree_t *tree);
+avl_t *avl_insert(avl_t **tree, int value);
+avl_t *array_to_avl(int *array, size_t size);
+avl_t *avl_remove(avl_t *root, int value);
+avl_t *sorted_array_to_avl(int *array, size_t size);
+int binary_tree_is_heap(const binary_tree_t *tree);
+heap_t *heap_insert(heap_t **root, int value);
+heap_t *array_to_heap(int *array, size_t size);
+int heap_extract(heap_t **root);
+int *heap_to_sorted_array(heap_t *heap, size_t *size);
 
-#endif /* _BINARY_TREES_H_ */
+#endif /* BINARY_TREES_H */
